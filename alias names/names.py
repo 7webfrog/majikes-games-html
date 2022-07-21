af009e06f24354e530c38c07f009eb27429da350
NoahFrahm <nofrahm@live.unc.edu>
2022-07-21 18:53:05 +0200

    alias names
    added alias names in new folder

---
+++ alias names/names.py
@@ -0,0 +1,25 @@
+
+adjectives = ["bald", "beautiful", "chubby", "clean",
+ "dazzling", "drab", "elegant", "fancy", "fit", "flabby",
+  "glamorous", "gorgeous", "handsome", "magnificent", "muscular",
+   "plain", "plump", "scruffv", "shapely", "skinny", "stockv", "unkempt",
+    "unsightly", "agreeable", "ambitious", "brave", "calm", "delightful",
+     "eager", "faithful", "gentle", "happy", "jolly", "kind", "livelv", "nice",
+      "obedient", "polite", "proud", "sillv", "thankful", "victorious", "witty", 
+      "wonderful", "zealous", "big", "colossal", "fat", "gigantic", "great", "huge",
+       "immense", "large", "little", "mammoth", "massive", "microscopic", "miniature",
+        "petite", "puny", "scrawny", "short", "small", "tall", "teenv", "tiny"]
+
+
+animals = ["Lion", "Tiger", "Giraffe", "Bear", "Zebra", "Panda", "Gorilla", "Monkey",
+ "Wolf", "Deer", "Fox", "Yak", "Jaguar", "Hyena", "Squirrel", "Elephant", "Crocodile",
+  "Kangaroo", "Rhinoceros", "Hippopotamus", "Blue whale", "Lion", "Giant Panda", "Cow",
+   "Dog", "Tiger", "Cat", "Wolverine", "Elephant", "Giraffe", "Horse", "Wolf", "Fox", 
+   "Bear", "Kangaroo", "Dog", "Cat", "Rabbit", "Turtle", "Fish", "Mouse", "Hamster",
+    "Chameleon", "Horse", "Pig", "Pigeon", "Parrot", "Snake", "Blue whale", "Shark",
+     "Fish", "Penguin", "Dolphin", "Octopus", "Seal", "Sealion", "Crab", "Piranha", 
+     "Sea horse", "Goldfish", "Walrus", "Prawns", "Catfish", "Eagle", "Pigeon", "Peacock",
+      "Crow", "Sparrow", "Parrot", "Owl", "Swan", "Bat", "Ostrich", "Woodpecker", "Crane", 
+      "Kingfisher", "Duck", "Hummingbird", "Swan", "Penguin", "Turkey", "Rooster", "Vulture",
+       "Cow", "Sheep", "Goat", "Pig", "Rabbit", "Duck", "Turkey", "Fish", "Horse", "Chicken"
+        "Bee", "Rooster", "Donkey", "Camel", "Buffalo"]
