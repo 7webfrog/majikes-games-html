66334797db86b546abcf7bd3a7d810a77d3ac9ad
NoahFrahm <nofrahm@live.unc.edu>
2022-07-22 14:49:42 +0200

    alias name changing
    removed duplicate animals and some adjectives

--- alias names/names.py
+++ alias names/names.py
@@ -1,25 +1,24 @@
 
 adjectives = ["bald", "beautiful", "chubby", "clean",
- "dazzling", "drab", "elegant", "fancy", "fit", "flabby",
+ "dazzling", "drab", "elegant", "fancy", "fit",
   "glamorous", "gorgeous", "handsome", "magnificent", "muscular",
-   "plain", "plump", "scruffv", "shapely", "skinny", "stockv", "unkempt",
+   "plain", "plump", "scruffy", "shapely",
     "unsightly", "agreeable", "ambitious", "brave", "calm", "delightful",
-     "eager", "faithful", "gentle", "happy", "jolly", "kind", "livelv", "nice",
-      "obedient", "polite", "proud", "sillv", "thankful", "victorious", "witty", 
+     "eager", "faithful", "gentle", "happy", "jolly", "kind", "lively", "nice",
+      "polite", "proud", "sillv", "thankful", "victorious", "witty", 
       "wonderful", "zealous", "big", "colossal", "fat", "gigantic", "great", "huge",
-       "immense", "large", "little", "mammoth", "massive", "microscopic", "miniature",
-        "petite", "puny", "scrawny", "short", "small", "tall", "teenv", "tiny"]
+       "immense", "large", "little", "mammoth", "massive", "microscopic", "miniature", "small", "tall", "tiny"]
 
 
-animals = ["Lion", "Tiger", "Giraffe", "Bear", "Zebra", "Panda", "Gorilla", "Monkey",
- "Wolf", "Deer", "Fox", "Yak", "Jaguar", "Hyena", "Squirrel", "Elephant", "Crocodile",
-  "Kangaroo", "Rhinoceros", "Hippopotamus", "Blue whale", "Lion", "Giant Panda", "Cow",
-   "Dog", "Tiger", "Cat", "Wolverine", "Elephant", "Giraffe", "Horse", "Wolf", "Fox", 
-   "Bear", "Kangaroo", "Dog", "Cat", "Rabbit", "Turtle", "Fish", "Mouse", "Hamster",
-    "Chameleon", "Horse", "Pig", "Pigeon", "Parrot", "Snake", "Blue whale", "Shark",
-     "Fish", "Penguin", "Dolphin", "Octopus", "Seal", "Sealion", "Crab", "Piranha", 
+animals = ["Lion", "Zebra", "Panda",
+ "Wolf", "Deer", "Fox", "Jaguar", "Hyena", "Squirrel", "Crocodile",
+  "Kangaroo", "Rhinoceros", "Hippopotamus", "Blue whale",
+   "Dog", "Tiger", "Cat", "Wolverine", "Elephant", "Giraffe",
+   "Bear", "Rabbit", "Turtle", "Mouse", "Hamster",
+    "Chameleon", "Horse", "Snake", "Shark",
+     "Fish", "Dolphin", "Octopus", "Seal", "Sealion", "Crab", "Piranha", 
      "Sea horse", "Goldfish", "Walrus", "Prawns", "Catfish", "Eagle", "Pigeon", "Peacock",
-      "Crow", "Sparrow", "Parrot", "Owl", "Swan", "Bat", "Ostrich", "Woodpecker", "Crane", 
-      "Kingfisher", "Duck", "Hummingbird", "Swan", "Penguin", "Turkey", "Rooster", "Vulture",
-       "Cow", "Sheep", "Goat", "Pig", "Rabbit", "Duck", "Turkey", "Fish", "Horse", "Chicken"
+      "Crow", "Sparrow", "Parrot", "Owl", "Bat", "Ostrich", "Woodpecker", "Crane", 
+      "Kingfisher", "Duck", "Hummingbird", "Swan", "Penguin", "Vulture",
+       "Cow", "Sheep", "Goat", "Pig", "Turkey", "Chicken"
         "Bee", "Rooster", "Donkey", "Camel", "Buffalo"]
